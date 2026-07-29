15bb4dad317c6c1e3048fdc23ba18c285ad66da0
Zion Resende <zionr0323@gmail.com>
2026-07-29 16:10:33 -0400

    no commands yet just exit

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+char version[10] = "1.0.0";
+char curcmd[1024];
+
+int main() {
+    printf("\nsheL - v%s\nunder GPL-3.0 license\n\n", version);
+
+    while (1) {
+        printf("%% ");
+
+        fgets(curcmd, sizeof(curcmd), stdin);
+
+        curcmd[strcspn(curcmd, "\n")] = 0;
+        if (strcmp(curcmd, "exit") == 0) exit(0);   
+    }
+}
+
