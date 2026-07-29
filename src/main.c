bf4e8e079d22344295c4ee0cbb42b045f7dbc44a
Zion Resende <zionr0323@gmail.com>
2026-07-29 16:14:17 -0400

    updated banner

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ char version[10] = "1.0.0";
 char curcmd[1024];
 
 int main() {
-    printf("\nsheL - v%s\nunder GPL-3.0 license\n\n", version);
+    printf("\nsheL - v%s\nunder GPL-3.0 license\nhttps://github.com/ZIZIES/sheL\n\n", version);
 
     while (1) {
         printf("%% ");
